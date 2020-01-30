30766b198834ee90c80630b35cc312fa18f46d0b
rsingh2489 <rsingh2489@gmail.com>
2020-01-30 12:27:21 +0530

    First Commit

---
+++ main/java/com/wbt/restfullwebservices/RestfullWebServicesApplication.java
@@ -0,0 +1,12 @@
+package com.wbt.restfullwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullWebServicesApplication.class, args);
+	}
+}
